6c9908b3058ff70564d589ff6d9a5e87faad488c
aveliino88 <89843361+aveliino88@users.noreply.github.com>
2024-05-22 09:15:49 +0300

    UI clean up 7

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -21,13 +21,13 @@ export default async function DynamicPage({ params }) {
 	const post = await getPost(params.slug);
 	return (
 		<>
-		<div className="container mt-10">
-			<div className="flex justify-center">
-			<img src={`${directus.url}assets/${post.image.filename_disk}?width=600`} alt="" />
+			<div className="container mt-10">
+				<div className="flex justify-center">
+					<img src={`${directus.url}assets/${post.image.filename_disk}?width=600`} alt="" />
+				</div>
+				<h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 flex justify-center mt-5">{post.title}</h2>
+				<div className="leading-7 [&:not(:first-child)]:mt-6" style={{ textAlign: "center" }} dangerouslySetInnerHTML={{ __html: post.content }}></div>
 			</div>
-			<h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 flex justify-center mt-5">{post.title}</h2>
-			<div className="leading-7 [&:not(:first-child)]:mt-6" style={{ textAlign: "center" }} dangerouslySetInnerHTML={{ __html: post.content }}></div>
-		</div>	
 		</>
 	);
 }
