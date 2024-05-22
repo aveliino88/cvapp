a3a22da0e487d6cae7f2fcfc773d1e82b482aa2a
aveliino88 <89843361+aveliino88@users.noreply.github.com>
2024-05-22 09:07:12 +0300

    UI clean up 5

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,7 +8,7 @@ export default function AboutPage() {
           <p className="leading-7 [&:not(:first-child)]:mt-6">Next.js is a React framework that allows you to build static and server-side rendered websites. It's a great tool for building fast, modern web applications.</p>
           <blockquote className="mt-6 border-l-2 pl-6 italic">Tailwind CSS is a utility-first CSS framework that allows you to build custom designs without writing custom CSS. It's a great tool for building responsive, mobile-first websites.</blockquote>
             <p className="capitalize">Together, Next.js and Tailwind CSS make a great combination for building modern web applications. I hope you enjoy my blog!</p>
-            <p className="text-sm text-muted-foreground">P.S This is generic text just for example purposes :-) </p>
+            <p className="text-sm text-muted-foreground">P.S This is generic text just for example purposes </p>
             </div>
       </div>
       </div>
